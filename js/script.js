bc180a6a0266229e45bedc7660adaba801662257
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-06 17:19:36 +0100

    bonus complete

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 
-const emailList = document.getElementById("user-emails")
 
-
-
-for(let i = 0; i < 10; i++){
-    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((response) => response.json())
-    .then ((data) => {
+const emailGenerator = () => {
     
-       
-        const email = data.response
-        
-        const itemList = document.createElement("li");
-
-        itemList.textContent = email;
-
-        emailList.appendChild(itemList);
-    
-    });
+    for(let i = 0; i < 10; i++){
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => response.json())
+        .then ((data) => {
+            
+            
+            const email = data.response
+            
+            const itemList = document.createElement("li");
+            
+            itemList.textContent = email;
+            
+            emailList.appendChild(itemList);
+            
+        });
+    }
 }
 
+
+const emailList = document.getElementById("user-emails");
+const addButton = document.getElementById("add-emails");
+addButton.addEventListener('click', emailGenerator)
