62072aa1628a4167cc1e9362abe7f961c8abe43b
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-06 16:52:03 +0100

    10 emails generated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 
+const emailList = document.getElementById("user-emails")
+let email;
 
-fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-.then((response) => response.json())
-.then ((data) => {
-    for(let i = 0; i < 10; i++){
-        console.table(data)
-    }
-});
+
+for(let i = 0; i < 10; i++){
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => response.json())
+    .then ((data) => {
+    
+       
+        email = data.response
+        console.log(email)
+    
+    });
+}
+
